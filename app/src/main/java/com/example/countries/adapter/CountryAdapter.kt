3f112f993e95efffee0a04318a3d94ec69fc5f6c
s0n03hp <sr.sankar.narayanan@walmart.com>
2022-01-09 07:11:13 +0530

    first render

--- app/src/main/java/com/example/countries/adapter/CountryAdapter.kt
+++ app/src/main/java/com/example/countries/adapter/CountryAdapter.kt
@@ -1,4 +1,6 @@
 package com.example.countries.adapter
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.countries.R
 
 import com.example.countries.modal.Country
+import com.squareup.picasso.Picasso
+import java.net.URL
 
 class CountryAdapter(val countryList:List<Country>?  ):RecyclerView.Adapter<CountryAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,7 +25,11 @@ class CountryAdapter(val countryList:List<Country>?  ):RecyclerView.Adapter<Coun
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val country= countryList?.get(position)
         if (country != null) {
-            holder.imageView.setImageURI(Uri.parse(country.flags.pngFlag))
+            Picasso.get().load(country.flags.pngFlag).into(holder.imageView)
+//            val `in` = URL(country.flags.pngFlag).openStream()
+//            val image: Bitmap = BitmapFactory.decodeStream(`in`)
+//            holder.imageView.setImageBitmap(image)
+//            holder.imageView.setImageURI(Uri.parse(country.flags.pngFlag))
             holder.textView.text=country.name.common
         }
 
